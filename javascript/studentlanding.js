966eb36e5da38c84f50ec97233e7736c57975191
TF777 <terrell@fordhorde.net>
2019-09-07 18:02:04 -0500

    fml

--- javascript/studentlanding.js
+++ javascript/studentlanding.js
@@ -1,11 +1,11 @@
 var map;
 var marker;
 var curLocation;
+var name;
+var phone;
 
 function pickMeUp() {
-    name = prompt("Enter your name:");
-    phonenum = prompt("Enter your phone number (numbers only):");
-    query = "name=" + name + "&phone=" + phonenum + "&currentlat=" + curLocation.lat + "&currentlong=" + curLocation.lng + "&destlat=" + marker.getPosition().lat() + "&destlong=" + marker.getPosition().lng();
+    query = "name=" + name + "&phone=" + phone + "&currentlat=" + curLocation.lat + "&currentlong=" + curLocation.lng + "&destlat=" + marker.getPosition().lat() + "&destlong=" + marker.getPosition().lng();
     
     fetch("http://localhost:7000?" + query, {
         method: "PUT",
@@ -18,6 +18,18 @@ function pickMeUp() {
     });
 }
 
+function start() {
+	initFields();
+	initMap();
+}
+
+function initFields() {
+	var x = document.createElement("INPUT");
+	x.setAttribute("type", "text");
+	x.setAttribute("value", "Hello World!");
+	document.body.appendChild(x);
+}
+
 function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -26,7 +38,7 @@ function initMap() {
 
 function showPosition(position) {
     curLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
-    map = new google.maps.Map(document.getElementById('map'), {zoom: 20, center: curLocation});
+    map = new google.maps.Map(document.getElementById('map'), {zoom: 19, center: curLocation});
     marker = new google.maps.Marker({position: curLocation, map: map});
     map.addListener('click', function(e) {
         changeMarker(e.latLng);
