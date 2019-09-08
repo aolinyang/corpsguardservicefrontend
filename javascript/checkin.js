77e50a1a2159d85c8681060c7704931892a7f5ad
VisionZ <willzhao2017@gmail.com>
2019-09-08 08:42:03 -0500

    Fixed Corps Response

--- javascript/checkin.js
+++ javascript/checkin.js
@@ -1,3 +1,39 @@
+let firstName;
+let lastName;
+let UIN;
+let email;
+let phone;
+let requesterPhone;
+
 function checkIn() {
-    
+    firstName = document.getElementById("fname").value;
+    lastName = document.getElementById("lname").value;
+    UIN = document.getElementById("uin").value;
+    email = document.getElementById("email").value;
+    phone = document.getElementById("phone").value;
+    requesterPhone = document.getElementById("rphone").value;
+
+    /*
+    Ask during Corps login who is the client they are responding to
+    */
+    let query =
+        "fname=" + firstName + "&" +
+        "lname=" + lastName + "&" +
+        "uin=" + UIN + "&" +
+        "email=" + email + "&" +
+        "phone=" + phone + "&" +
+        "rphone=" + requesterPhone;
+
+    alert(query);
+
+    fetch("http://localhost:7071/api/HttpTrigger" + "?" + query, {
+        method: "POST",
+        headers: {
+            accept: "application/json"
+        }
+    }).then(function (res) {
+        return res.json()
+    }).then(function (res) {
+
+    });
 }
