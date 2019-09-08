6f8df8f30c4b1fea24f2350327791a32cf973779
VisionZ <willzhao2017@gmail.com>
2019-09-08 00:06:09 -0500

    Adjusted Endpoints to Local Azure Function

--- javascript/studentlanding.js
+++ javascript/studentlanding.js
@@ -11,32 +11,32 @@ function pickMeUp() {
     phone = document.getElementById("phonenum").value;
     query = "fname=" + fname + "&lname=" + lname + "&phone=" + phone + "&currentlat=" + curLocation.lat + "&currentlong=" + curLocation.lng + "&destlat=" + marker.getPosition().lat() + "&destlong=" + marker.getPosition().lng();
     alert(query);
-    fetch("http://localhost:7000?" + query, {
-        method: "PUT",
+    fetch("http://localhost:7071/api/HttpTrigger?" + query, {
+        method: "POST",
         headers: {
             accept: "application/json"
         }
-    }).then(function(res) {return res.json()})
-    .then(function(res) {
-        
-    });
+    }).then(function (res) { return res.json() })
+        .then(function (res) {
+
+        });
 }
 
 function start() {
-	initMap();
+    initMap();
 }
 
 function initMap() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, error, {enableHighAccuracy: true});
+        navigator.geolocation.getCurrentPosition(showPosition, error, { enableHighAccuracy: true });
     }
 }
 
 function showPosition(position) {
-    curLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
-    map = new google.maps.Map(document.getElementById('map'), {zoom: 19, center: curLocation});
-    marker = new google.maps.Marker({position: curLocation, map: map});
-    map.addListener('click', function(e) {
+    curLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+    map = new google.maps.Map(document.getElementById('map'), { zoom: 19, center: curLocation });
+    marker = new google.maps.Marker({ position: curLocation, map: map });
+    map.addListener('click', function (e) {
         changeMarker(e.latLng);
     });
 }
